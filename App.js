4e5ba3a305fc033fda46800d9ecafeef0fa7e1c2
DAVID\Santi <Santiagomora500@gmail.com>
2024-02-18 19:23:41 -0500

    Se realizó adelanto y pruebas de la pantalla principal

--- App.js
+++ App.js
@@ -1,26 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-paper";
-import { Chip } from "react-native-paper";
+import { StyleSheet,  View } from "react-native";
+import { Avatar, Button, Card, Text, Chip } from "react-native-paper";
+
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#cf2828",
-      }}
-    >
+    <View style={{ flex: 1, backgroundColor: "#265073" }}>
       <View style={styles.container}>
         <Chip elevation={5} onPress={() => console.log("Pressed")}>
           Principal
         </Chip>
         <Chip elevation={5} onPress={() => console.log("Pressed")}>
-          Principal
+          Libros 
         </Chip>
         <Chip elevation={5} onPress={() => console.log("Pressed")}>
-          Principal
+          Preferencias
         </Chip>
+      </View>
+
+      <View style={styles.card}>
+        <Card>
+          <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+          <Card.Content>
+            <Text variant="titleLarge">Card title</Text>
+            <Text variant="bodyMedium">Card content</Text>
+          </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+          <Card.Actions>
+            <Button onPress={() => console.log("Puto el que presione")}> Cancel </Button>
+            <Button onPress={() => console.log("Puto el que presione")}> Ok </Button>
+          </Card.Actions>
+        </Card>
       </View>
 
       <Text>Hello world!!</Text>
@@ -31,9 +42,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     flexDirection: "row",
-    alignItems: "center",
+    alignItems: "flex-start",
+    marginTop:40,
+    gap: 35,
     justifyContent: "center",
   },
+
+  card:{
+    marginTop: 50,
+  }
+
 });
