ccbc73acbe77a38d90bfb8529255b4683cb8c157
Juandavidg123 <juandavidgodoy@hotmail.com>
2024-03-01 19:56:46 -0500

    created new database

--- backend/db/createtable.py
+++ backend/db/createtable.py
@@ -1,23 +1,17 @@
 #Fichero para crear las tablas de la base de datos, solo ejecutar este fichero en caso de que no este creada la tabla
 
-
 import psycopg2
+from connection import conn, cur
 
 try:
-    conn = psycopg2.connect(
-        "postgres://linword_user:mqd4wIWFLQAqI0XSQoEQhBLjDeEtqbQW@dpg-cncijceg1b2c739i83jg-a.oregon-postgres.render.com/linword"
-    )
-    cur = conn.cursor()
     query = '''
         CREATE TABLE IF NOT EXISTS users (
-  	        cedula int NOT NULL,
-	        username varchar(255) NOT NULL,
-  	        password varchar(255) NOT NULL,
-  	        childName varchar(255) NOT NULL,
-	        codeChild int NOT NULL UNIQUE,
-	        role varchar(255) CHECK(role IN ('parent', 'child')), 
-  	        PRIMARY KEY (cedula)
-        ); 
+  	    cedula int NOT NULL,
+	    correo varchar(255) NOT NULL,
+  	    password varchar(255) NOT NULL,
+  	    childName varchar(255) NOT NULL,
+  	    PRIMARY KEY (cedula)
+    ); 
     '''
     cur.execute(query)
     conn.commit()
--- backend/db/db.sql
+++ backend/db/db.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
   	cedula int NOT NULL,
-	username varchar(255) NOT NULL,
+	correo varchar(255) NOT NULL,
   	password varchar(255) NOT NULL,
   	childName varchar(255) NOT NULL,
-	codeChild int NOT NULL UNIQUE,
-	role varchar(255) CHECK(role IN ('parent', 'child')), 
   	PRIMARY KEY (cedula)
 ); 
