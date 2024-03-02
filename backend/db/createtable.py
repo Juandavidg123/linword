47c46703b1db5e9fdccdeaada2820f0204e3802e
Juandavidg123 <juandavidgodoy@hotmail.com>
2024-03-01 20:33:45 -0500

    added check to email

--- backend/db/createtable.py
+++ backend/db/createtable.py
@@ -6,12 +6,12 @@ from connection import conn, cur
 try:
     query = '''
         CREATE TABLE IF NOT EXISTS users (
-  	    cedula int NOT NULL,
-	    correo varchar(255) NOT NULL,
-  	    password varchar(255) NOT NULL,
-  	    childName varchar(255) NOT NULL,
-  	    PRIMARY KEY (cedula)
-    ); 
+        cedula int NOT NULL,
+        correo varchar(255) NOT NULL UNIQUE CHECK (correo ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+        password varchar(255) NOT NULL,
+        childName varchar(255) NOT NULL,
+        PRIMARY KEY (cedula)
+        );
     '''
     cur.execute(query)
     conn.commit()
--- backend/db/db.sql
+++ backend/db/db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
   	cedula int NOT NULL,
-	correo varchar(255) NOT NULL,
+	correo varchar(255) NOT NULL UNIQUE CHECK (correo ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
   	password varchar(255) NOT NULL,
   	childName varchar(255) NOT NULL,
   	PRIMARY KEY (cedula)
